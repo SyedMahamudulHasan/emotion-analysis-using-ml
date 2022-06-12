42bc50c725a858bf2583c5be12e0fec96b664785
omedia23 <syedhasan.cse@gmail.com>
2022-06-12 21:35:41 +0600

    fixed bugs

--- main.py
+++ main.py
@@ -3,48 +3,43 @@ from PIL import ImageTk, Image
 from tkinter.ttk import *
 
 
-class MyApp():
-    def __init__(self):
-        self.root = Tk()
 
-        self.root.geometry("700x500")
+root = Tk()
 
-        # title of the applications
-        self.root.title("Mental Health Analysis Using ML")
+root.geometry("700x500")
 
-        # defining the frame
-        frame = Frame(self.root, width=600, height=800)
-        frame.place(anchor='center', relx=0.5, rely=0.5)
+# title of the applications
+root.title("Mental Health Analysis Using ML")
 
-        # title
-        Label(self.root, text = 'Check Your Mental Helth', font =(
-          'Verdana', 35)).pack(side = TOP, pady = 10)
+# defining the frame
+frame = Frame(root, width=600, height=800)
+frame.place(anchor='center', relx=0.5, rely=0.5)
 
-        # image
-        img = ImageTk.PhotoImage(Image.open('mentalimage.webp'))
-        label = Label(frame, image=img)
-        label.pack()
+# title
+Label(root, text = 'Check Your Mental Helth', font =('Verdana', 35)).pack(side = TOP, pady = 10)
+
+# image
+img = ImageTk.PhotoImage(Image.open('mentalimage.webp'))
+label = Label(frame, image=img)
+label.pack()
 
 
-        # This will create style object
-        style = Style()
+# This will create style object
+style = Style()
 
-        # This will be adding style, and
-        # naming that style variable as
-        # W.Tbutton (TButton is used for ttk.Button).
-        style.configure('W.TButton', font =
-                       ('calibri', 20, 'bold',),
-                        foreground = 'blue')
+# This will be adding style, and
+# naming that style variable as
+# W.Tbutton (TButton is used for ttk.Button).
+style.configure('W.TButton', font =('calibri', 20, 'bold',),foreground = 'blue')
 
-        # Style will be reflected only on
-        # this button because we are providing
-        # style only on this Button.
-        ''' Button'''
-        btn = Button(self.root, text = 'Start!',
-                        style = 'W.TButton',
-                     command = self.root.destroy).pack(side=BOTTOM, pady=28)
+# Style will be reflected only on
+# this button because we are providing
+# style only on this Button.
+''' Button'''
+btn = Button(root, text = 'Start!',
+                            style = 'W.TButton',
+                         command = root.destroy).pack(side=BOTTOM, pady=28)
 
 
 
-app = MyApp()
-app.root.mainloop()
+root.mainloop()
